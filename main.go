c735f86b62ac677321ffd72f8f5cde591b108697
Prakhar Srivastav <srivprakhar@gmail.com>
2019-06-27 10:14:22 +0200

    Completed the demo

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+	"context"
+	"fmt"
 	"net/http"
 	"time"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"github.com/labstack/echo"
+	"google.golang.org/api/option"
 )
 
+var client *auth.Client
+
 func main() {
+	var err error
+	var app *firebase.App
 	e := echo.New()
 	e.HideBanner = true
+
+	// create firebase client
+	opt := option.WithCredentialsFile("fb.json")
+	if app, err = firebase.NewApp(context.Background(), nil, opt); err != nil {
+		e.Logger.Fatalf("error initializing app: %v", err)
+	}
+
+	if client, err = app.Auth(context.Background()); err != nil {
+		e.Logger.Fatalf("error creating firebase client: %v", err)
+	}
+
 	e.Static("/", "public")
 
 	grp := e.Group("/api/")
@@ -22,12 +43,28 @@ func serviceA(ctx echo.Context) error {
 	idToken := ctx.Request().Header.Get("id-token")
 	if idToken == "" {
 		return ctx.JSON(200, map[string]string{
-			"error": "no token",
+			"error":   "no token",
+			"details": "id-token missing in header",
 		})
 	}
+
+	t, err := client.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		return ctx.JSON(200, map[string]string{
+			"error":   "invalid token",
+			"message": " invalid id-token provided",
+		})
+	}
+
+	fmt.Println("id-token verified")
+
 	resp := make(map[string]string)
 	resp["name"] = "Service A"
 	resp["time"] = time.Now().Format(time.UnixDate)
+	resp["uid"] = t.UID
+	resp["project-url"] = t.Issuer
+	resp["project-id"] = t.Audience
+
 	return ctx.JSON(200, resp)
 }
 func serviceB(ctx echo.Context) error {
@@ -37,9 +74,22 @@ func serviceB(ctx echo.Context) error {
 			"error": "no token",
 		})
 	}
+	t, err := client.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		return ctx.JSON(200, map[string]string{
+			"error":   "invalid token",
+			"message": " invalid id-token provided",
+		})
+	}
+	fmt.Println("id-token verified")
+
 	resp := make(map[string]string)
 	resp["name"] = "Service B"
 	resp["time"] = time.Now().Format(time.UnixDate)
+	resp["uid"] = t.UID
+	resp["project-url"] = t.Issuer
+	resp["project-id"] = t.Audience
+
 	return ctx.JSON(200, resp)
 }
 
